c88dcedb67946ac74601207334e7fb0f08d35259
Nick White <nick.white@rockabox.com>
2014-01-10 11:02:24 +0000

    Initial setup
    - Adds Bootstrap, jQuery
    - Adds two json call files
    - Adds Bower

---
+++ jsonCalls/metric-def.js
@@ -0,0 +1,60 @@
+var stats = [
+    {
+        columns: [
+            {
+                id: 1,
+                name: 'aggregateBackupImage',
+                humanName: 'Aggregate Backup Image',
+                type: 'int'
+            },
+            {
+                id: 2,
+                name: 'uniqueClicks',
+                humanName: 'Unique Clicks',
+                type: 'int'
+            },
+            {
+                id: 3,
+                name: 'aggregateCTA',
+                humanName: 'Aggregate CTAs',
+                type: 'int'
+            }
+        ]
+    },
+    {
+        id: 1,
+        name: 'Royal Carribean Creative',
+        stats: [
+            {
+                metricId: 1,
+                total: 25432
+            },
+            {
+                metricId: 2,
+                total: 1210
+            },
+            {
+                metricId: 3,
+                total: 12134
+            }
+        ],
+    },
+    {
+        id: 2,
+        name: 'AOL Placement',
+        stats: [
+            {
+                metricId: 1,
+                total: 25432
+            },
+            {
+                metricId: 3,
+                total: 1210
+            },
+            {
+                metricId: 2,
+                total: 12134
+            }
+        ]
+    }
+];
